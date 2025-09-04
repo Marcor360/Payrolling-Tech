3cd63133be5d36d48fe2f7b689725bed91cf9a34
Marco Antonio Rulfo Castro <81320236+Marcor360@users.noreply.github.com>
2025-09-04 11:51:07 -0600

    1.3.0

---
+++ src/pages/subpages/Nominas.tsx
@@ -0,0 +1,11 @@
+import Headers from "../../components/header.tsx"
+import Foter from "../../components/footer.tsx"
+export default function Nominas() {
+    return (
+        <>
+            <Headers />
+            <h1>Nomina</h1>
+            <Foter />
+        </>
+    )
+}
